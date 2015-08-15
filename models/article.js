6f4bf4fb6f425be430d2d135bca24e3a9a4e81bf
Yarygin <yarygin.alexander@inbox.com>
2015-08-15 19:55:56 +0300

    допилил монго

---
+++ models/article.js
@@ -0,0 +1,85 @@
+var mongoose = require('mongoose');
+
+module.exports = function (db) {
+
+    var ArticleSchema = new mongoose.Schema({
+        id: Number,
+        title: String,
+        date: Date,
+        preview: String,
+        fulltext: String,
+        image: String
+    });
+    var Article = mongoose.model('news', ArticleSchema);
+  // Список документов
+  var list = function (req, res, next) {
+      Article.find({}, function (err, data) {
+          if (err) next(err);
+          res.send(data);
+      });
+  };
+
+  // Один документ
+  var get = function (req, res, next) {
+    //try{var id = mongoose.Types.ObjectId(req.params.id)}
+    //catch (e){res.send(400)}
+    var id = req.params.id;
+      console.log("!!!!"+id);
+      Article.find({_id: id}, function (err, data) {
+      if (err) next(err);
+      if (data) {
+        res.send(data);
+      } else {
+        res.send(404);
+      }
+    })
+  };
+
+  // Создаем документ
+  var create = function (req, res, next) {
+
+      Article.create(req.body, function (err, data) {
+      if (err) {
+        next(err);
+      }
+      res.send(data);
+    });
+  };
+
+  // Обновляем документ
+  var update = function (req, res, next) {
+    try{var id = mongoose.Types.ObjectId(req.params.id)}
+    catch (e){res.send(400)}
+
+      Article.update({_id: id}, {$set: req.body}, function (err, numberAffected, data) {
+      if (err) next(err);
+
+      if (numberAffected) {
+        res.send(200);
+      } else {
+        res.send(404);
+      }
+
+    })
+  };
+
+  // Удаляем документ
+  var remove = function (req, res, next) {
+    try{var id = mongoose.Types.ObjectId(req.params.id)}
+    catch (e){res.send(400)}
+
+      Article.remove({_id: id}, function (err, data) {
+      if (err) next(err);
+      res.send(data ? req.params.id : 404);
+    });
+  };
+
+  return {
+    model : Article,
+    list  : list,
+    get   : get,
+    create: create,
+    update: update,
+    remove: remove
+  }
+};
--- server.js
+++ server.js
@@ -4,6 +4,13 @@ var http = require('http').Server(app);
 var appRouter = require('./routes/app');
 var apiRouter = require('./routes/api');
 var appPort = 3000;
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/angular_site');
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function (callback) {
+    console.log('connection opened');
+});
 
 app.use(appRouter).use(apiRouter);
 
