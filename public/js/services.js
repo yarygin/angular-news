61404808e7f9ba727c6e41a6df3fdb5485c1e22c
Yarygin <yarygin.alexander@inbox.com>
2015-08-15 20:57:16 +0300

    делаю роутинг

--- public/js/services.js
+++ public/js/services.js
@@ -2,13 +2,9 @@ var siteServices = angular.module('siteServices', ['ngResource']);
 
 siteServices.factory('News', ['$resource',
     function($resource){
-        console.log($resource);
         return $resource('/api/news/:articleId', {}, {
             query: {
-                method:'GET', 
-                params:{
-                    articleId:'news'
-                }, 
+                method:'GET',
                 isArray:true
             }
         });
